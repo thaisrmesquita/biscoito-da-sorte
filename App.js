78739d8730ba4f8fe41c6d655514bee6478b5194
Thais RIbeiro <thaisribeiro.mesquita@gmail.com>
2020-10-04 18:13:28 -0300

    Adicionado estado para alteração de texto e imagem quando clicado o botão

--- App.js
+++ App.js
@@ -2,15 +2,41 @@ import React, { Component } from 'react';
 import { View, StyleSheet, Image, Text, TouchableOpacity } from 'react-native';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      textoFrase: '"Siga os bons e aprenda com eles."',
+      img: require('./src/biscoito.png')
+    };
+
+    this.quebraBiscoito = this.quebraBiscoito.bind(this);
+
+    this.frases = [
+      'Siga os bons e aprenda com eles.',
+      'O bom senso vale mais do que muito conhecimento.',
+      'O riso é a menor distância entre duas pessoas.',
+      'Deixe de lado as preocupações e seja feliz.',
+      'Realize o óbvio, pense no improvável e conquiste o impossível.',
+      'Acredite em milagres, mas não dependa deles.',
+      'A maior barreira para o sucesso é o medo do fracasso.'
+    ];
+  }
+  quebraBiscoito(){
+    let numeroAleatorio = Math.floor(Math.random() * this.frases.length);
+    this.setState({
+      textoFrase: ' "' + this.frases[numeroAleatorio] + '" ',
+      img: require('./src/biscoitoAberto.png')
+    })
+  }
   render () {
     return (
       <View style = {styles.container}>
         <Image
-          source={require('./src/biscoito.png')} 
+          source={this.state.img} 
           style={styles.img}
         />
-        <Text style={styles.textoFrase}>"Alguma frase aqui"</Text>
-        <TouchableOpacity style={styles.botao}>
+        <Text style={styles.textoFrase}>{this.state.textoFrase}</Text>
+        <TouchableOpacity style={styles.botao} onPress={this.quebraBiscoito}>
           <View style={styles.btnArea}>
             <Text style={styles.btnTexto}>Quebrar Biscoito</Text>
           </View>
